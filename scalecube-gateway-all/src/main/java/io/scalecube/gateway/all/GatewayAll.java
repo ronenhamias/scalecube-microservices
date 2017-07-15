33d40aa7314a895bde6758aa0818fe068acdb60e
ronen <ronen.hamias@gmail.com>
2017-07-15 15:02:28 +0300

    working on packaging for docker and exposing configuration

--- scalecube-gateway-all/src/main/java/io/scalecube/gateway/all/GatewayAll.java
+++ scalecube-gateway-all/src/main/java/io/scalecube/gateway/all/GatewayAll.java
@@ -10,11 +10,14 @@ import io.scalecube.services.Microservices;
 import org.rapidoid.setup.On;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.Properties;
 
 public class GatewayAll {
 
@@ -22,26 +25,53 @@ public class GatewayAll {
    * app main.
    * 
    * @param args application arguments.
+   * @throws IOException on error.
    */
-  public static void main(String[] args) {
-    RedissonClient client = Redisson.create();
-    RedisOrganizations accountManager = new RedisOrganizations(client);
-    accountManager.clearAll();
+  public static void main(String[] args) throws IOException {
+
+    Info info = new Info();
+
+    Config config = new Config();
+    config.useSingleServer()
+        .setAddress(info.redisAddress())
+        .setConnectionPoolSize(10);
+
+    RedissonClient client = Redisson.create(config);
 
     Microservices seed = Microservices.builder().build();
+    String address = seed.cluster().address().host();
+    RedisOrganizations accountManager = new RedisOrganizations(client);
+    accountManager.clearAll();
 
     RedisAccountService account = new RedisAccountService(client);
     RedisConfigurationService configuration = new RedisConfigurationService(client);
 
-    Microservices.builder()
+    Microservices.builder().port(4801)
         .services(account, configuration)
         .seeds(seed.cluster().address())
         .build();
 
     AccountGateway.start(8080, seed);
     ConfigurationGateway.start(8080, seed);
-    On.port(8080).get("/").html(www(args[0]));
+    if (args.length == 0) {
+      On.port(8080).get("/").html(www(null));
+    } else {
+      On.port(8080).get("/").html(www(args[0]));
+    }
+
+
+    Logo.builder().tagVersion(info.version())
+        .port("4801")
+        .ip(address)
+        .group(info.groupId())
+        .artifact(info.artifactId())
+        .javaVersion(info.java())
+        .osType(info.os())
+        .pid(info.pid())
+        .website().draw();
   }
+
+
 
   private static String www(String workFir) {
     try {
@@ -59,5 +89,6 @@ public class GatewayAll {
     }
     return null;
   }
+
 
 }
---
+++ scalecube-gateway-all/src/main/java/io/scalecube/gateway/all/Logo.java
@@ -0,0 +1,120 @@
+package io.scalecube.gateway.all;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Logo {
+
+  public static class LogoHeader {
+    private static final String space = "        ";
+    String value;
+
+    public LogoHeader(String value) {
+      this.value = space + value;
+    }
+
+    public String value() {
+      return this.value;
+    }
+  }
+
+  public static class Builder {
+    private int index = 0;
+    private int startAt = 7;
+    private Map<Integer, LogoHeader> headers = new HashMap<>();
+
+    public Builder ip(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("IP Address: #1".replaceAll("#1", value)));
+      return this;
+    }
+    
+    public Builder tagVersion(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("ScaleCube #1 is Running.".replaceAll("#1", value)));
+      return this;
+    }
+
+    public Builder group(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("Group: #1".replaceAll("#1", value)));
+      return this;
+    }
+
+    public Builder artifact(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("Artifact: #1".replaceAll("#1", value)));
+      return this;
+    }
+
+    public Builder port(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("Port: #1".replaceAll("#1", value)));
+      return this;
+    }
+
+    public Builder osType(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("OS: #1".replaceAll("#1", value)));
+      return this;
+    }
+
+    public Builder pid(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("PID: #1".replaceAll("#1", value)));
+      return this;
+    }
+    
+    public Builder javaVersion(String value) {
+      headers.put(startAt + headers.size() + 1, new LogoHeader("Java: #1".replaceAll("#1", value)));
+      return this;
+    }
+
+    public Builder website() {
+      headers.put(startAt + headers.size() + 2, new LogoHeader("      http://scalecube.io"));
+      return this;
+    }
+
+    /**
+     * draw the scalecube logo.
+     */
+    public void draw() {
+      pln("                         .,,,,,,                          ");
+      pln("                       .,,,,,,,,,,,,/                     ");
+      pln("                  .,,,,,,,,,,,,,,,,,,,.                   ");
+      pln("               .,,,,,,,,,,,,,,,,,,,,,,,,,,,.              ");
+      pln("            .,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.           ");
+      pln("         ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,        ");
+      pln("     ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,     ");
+      pln("     *  ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,*       ");
+      pln("     ***** /,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,/  ....    ");
+      pln("     ******** /*,,,,,,,,,,,,,,,,,,,,,,,,,,,,/  .......    ");
+      pln("     ***********, /,,,,,,,,,,,,,,,,,,,,,*  ...........    ");
+      pln("     ***************. *,,,,,,,,,,,,,*/ ...............    ");
+      pln("     ******************, /*,,,,,,/  ..................    ");
+      pln("     ********************** /*/  .....................    ");
+      pln("     ************************ ........................    ");
+      pln("     ************************ ........................    ");
+      pln("     ************************ ........................    ");
+      pln("     ************************ ........................    ");
+      pln("     ************************ ........................    ");
+      pln("     ************************ ........................    ");
+      pln("     ************************ ........................    ");
+      pln("     ************************ ........................    ");
+      pln("         ******************** ....................        ");
+      pln("            ***************** .................           ");
+      pln("                ************* .............               ");
+      pln("                   ********** ..........                  ");
+      pln("                       ****** ......                      ");
+      pln("                          *** ...                         ");
+    }
+
+    private void pln(String line) {
+      LogoHeader logoHeader = headers.get(index);
+      if (logoHeader != null) {
+        System.out.println(line + logoHeader.value());
+      } else {
+        System.out.println(line);
+      }
+      index++;
+    }
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+}
+

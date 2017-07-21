b86d1984ff147a0b8d9be040d6bcd17b32a6db99
ronen <ronen.hamias@gmail.com>
2017-07-21 06:51:24 +0300

    create deploy script on master

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"; fi
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-seed; fi
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-gateway-all; fi
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-configuration-gateway; fi
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-configuration; fi
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-account-gateway; fi
+if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-account; fi

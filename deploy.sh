85f411cf2b22bb00f244ed7ae1ef7075ac226134
ronen <ronen.hamias@gmail.com>
2017-07-21 07:55:40 +0300

    remove commas

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"; 
-bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-seed; 
-bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-gateway-all; 
-bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-configuration-gateway; 
-bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-configuration; 
-bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-account-gateway; 
-bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-account; 
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" 
+bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-seed
+bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-gateway-all 
+bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-configuration-gateway
+bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-configuration
+bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-account-gateway 
+bash docker_build.sh $DOCKER_USERNAME $TRAVIS_BUILD_DIR scalecube-account

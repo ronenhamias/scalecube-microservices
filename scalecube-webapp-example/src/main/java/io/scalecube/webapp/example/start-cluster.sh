18f2ad38c83ec3b36c0ccc03865dc9caab6e49a5
ronen <ronen@ronen-pc.pisoft.local>
2017-07-24 03:20:18 +0300

    change the example ip and add a bash script to start all containers

---
+++ scalecube-webapp-example/src/main/java/io/scalecube/webapp/example/start-cluster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+VERSION="0.0.5-SNAPSHOT"
+
+dockip() {
+  echo $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $(docker ps -aq | head -n1))
+}
+
+docker run -d -t redis
+
+REDIS_IP=$(dockip)
+echo "redis ip: $REDIS_IP"
+
+docker run -d -t ronenna/scalecube:scalecube-seed-0.0.5-SNAPSHOT
+
+SEED_IP=$(dockip)
+echo "seed ip: $SEED_IP"
+
+docker run -d -e "SC_SEED_ADDRESS=$SEED_IP:4801" -t ronenna/scalecube:scalecube-seed-$VERSION
+
+docker run -d -e "SC_SEED_ADDRESS=$SEED_IP:4801" -e "REDIS_ADDRESS=redis://$REDIS_IP:6379" -t ronenna/scalecube:scalecube-account-$VERSION
+
+docker run -d -e "SC_SEED_ADDRESS=$SEED_IP:4801" -e "REDIS_ADDRESS=redis://$REDIS_IP:6379" -t ronenna/scalecube:scalecube-configuration-$VERSION
+
+docker run -d -e "SC_SEED_ADDRESS=$SEED_IP:4801" -p 8081:8081 -t ronenna/scalecube:scalecube-account-gateway-$VERSION
+
+docker run -d -e "SC_SEED_ADDRESS=$SEED_IP:4801" -p 8082:8081 -t ronenna/scalecube:scalecube-configuration-gateway-$VERSION

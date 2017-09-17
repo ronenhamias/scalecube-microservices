77260094e4a19373d075fb940f9092a6aeee10c9
ronen <ronen@ronen-ubuntu>
2017-09-17 15:52:29 +0300

    echo image

--- docker_build.sh
+++ docker_build.sh
@@ -6,8 +6,9 @@ build(){
   cd $BUILD_DIR/$ARTIFACT
   MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   DOCKER_IMAGE=$DOCKER_NAMESPACE/$DOCKER_REPO:$ARTIFACT-$MVN_VERSION
-  docker build -t $DOCKER_IMAGE .
-  docker push $DOCKER_IMAGE
+  echo "docker image: " $DOCKER_IMAGE
+  docker build -t $DOCKER_NAMESPACE/$DOCKER_REPO:$ARTIFACT-$MVN_VERSION .
+  docker push $DOCKER_NAMESPACE/$DOCKER_REPO:$ARTIFACT-$MVN_VERSION
   cd $BUILD_DIR
 }
 

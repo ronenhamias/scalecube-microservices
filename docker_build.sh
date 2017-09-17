0ca8565320c984d63f4fc75e1784d34a3bd58951
ronen <ronen@ronen-ubuntu>
2017-09-17 17:51:45 +0300

    add echo

--- docker_build.sh
+++ docker_build.sh
@@ -2,6 +2,7 @@
 ARTIFACT=$1
 
 build(){
+  echo "current dir:  $PWD, travis build dir: $TRAVIS_BUILD_DIR"
   docker images
   cd $TRAVIS_BUILD_DIR/$ARTIFACT
   MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)

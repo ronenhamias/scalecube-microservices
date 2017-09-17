c9b7fc2141b05fd45fb61207b12332c1e2a7f9c1
ronen <ronen@ronen-ubuntu>
2017-09-17 17:33:41 +0300

    use travis build dir

--- docker_build.sh
+++ docker_build.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-BUILD_DIR=$TRAVIS_BUILD_DIR
 ARTIFACT=$1
 
 build(){
   docker images
-  cd $BUILD_DIR/$ARTIFACT
+  cd $TRAVIS_BUILD_DIR/$ARTIFACT
   MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   DOCKER_IMAGE=$DOCKER_NAMESPACE/$DOCKER_REPO:$ARTIFACT-$MVN_VERSION
   echo "docker image: " $DOCKER_IMAGE
   docker image build -t $DOCKER_IMAGE .
   docker images
   docker push $DOCKER_IMAGE
-  cd $BUILD_DIR
+  cd $TRAVIS_BUILD_DIR
 }
 
 build

259a1369af371d3cefc271fa70d83301c6454115
ronen nachmias <ronen.hamias@gmail.com>
2017-09-18 17:15:20 +0300

    fix deploy script

--- docker_build.sh
+++ docker_build.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 ARTIFACT=$1
 
-build(){
-  echo "current dir:  $PWD, travis build dir: $TRAVIS_BUILD_DIR"
-  ls
+build() {
   cd $TRAVIS_BUILD_DIR/$ARTIFACT
-  echo "current dir:  $PWD"
   
   MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
   
   DOCKER_IMAGE=$DOCKER_NAMESPACE/$DOCKER_REPO:$ARTIFACT-$MVN_VERSION
-  echo "docker image: " $DOCKER_IMAGE
-  echo "target/lib dir: " 
-  ls 
-  ls target
-  ls target/lib
-
-  docker image build -t $DOCKER_IMAGE .
-  docker images
+  echo "pushing docker image: " $DOCKER_IMAGE
   docker push $DOCKER_IMAGE
   cd $TRAVIS_BUILD_DIR
-  echo "current dir:  $PWD"
+  
 }
 
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 build

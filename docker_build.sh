d1316c7c88d2bf8f1a914a1bed9a57b010a529e4
ronen nachmias <ronen.hamias@gmail.com>
2017-09-18 07:49:19 +0300

    ls root

--- docker_build.sh
+++ docker_build.sh
@@ -3,9 +3,12 @@ ARTIFACT=$1
 
 build(){
   echo "current dir:  $PWD, travis build dir: $TRAVIS_BUILD_DIR"
+  ls
   cd $TRAVIS_BUILD_DIR/$ARTIFACT
   echo "current dir:  $PWD"
+  
   MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+  
   DOCKER_IMAGE=$DOCKER_NAMESPACE/$DOCKER_REPO:$ARTIFACT-$MVN_VERSION
   echo "docker image: " $DOCKER_IMAGE
   echo "target/lib dir: " 

658e978c46e62b3912aa29e009757e4deca81fda
Dmytro Lazebnyi <dmytro.lazebnyi@jivygroup.com>
2018-05-15 12:18:19 +0300

    Add default constructors for serialization/deserialization purposes

--- scalecube-account-api/src/main/java/io/scalecube/account/api/CreateOrganizationResponse.java
+++ scalecube-account-api/src/main/java/io/scalecube/account/api/CreateOrganizationResponse.java
@@ -2,6 +2,11 @@ package io.scalecube.account.api;
 
 public class CreateOrganizationResponse extends OrganizationInfo {
 
+  /**
+   * @deprecated only for serialization/deserialization
+   */
+  CreateOrganizationResponse() {}
+
   public CreateOrganizationResponse(String id, String name, ApiKey[] apiKey, String email, String ownerId) {
     super(id, name, apiKey, email, ownerId);
   }
